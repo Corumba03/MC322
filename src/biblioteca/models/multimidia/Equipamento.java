d84dd13574e21e21fa6b9dfa3ae89ece7cfa0bd1
Seu Nome <seu@email.com>
2023-09-06 20:56:08 -0300

    Esqueleto da classe Equipamento

---
+++ src/biblioteca/models/multimidia/Equipamento.java
@@ -0,0 +1,20 @@
+package biblioteca.models.multimidia;
+
+public class Equipamento {
+	
+	public class Informatica {
+		// TODO Detalhes de computadores, tablets, projetores, etc.
+	}
+	
+	public class Audiovisual {
+		// TODO Equipamentos como câmeras, microfones, caixas de som, etc.
+	}
+	
+	public class Impressao {
+		// TODO Impressoras, scanners, copiadoras, etc.
+	}
+	
+	enum CategoriaEquipamento {
+		INFORMATICA, AUDIOVISUAL, IMPRESSAO
+	}
+}
