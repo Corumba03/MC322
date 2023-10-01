6d8c3c459e21c2031355679d6445b7366ed0a533
Corumba03 <m236730@dac.unicamp.br>
2023-10-01 00:18:18 -0300

    Continuação do lab 08

--- src/biblioteca/models/membros/funcionarios/Funcionario.java
+++ src/biblioteca/models/membros/funcionarios/Funcionario.java
@@ -1,6 +1,10 @@
 package biblioteca.models.membros.funcionarios;
 
+import bibexceptions.AcessoNegadoException;
+import bibexceptions.ItemInvalidoException;
+import biblioteca.models.Biblioteca;
 import biblioteca.models.membros.Membro;
+import biblioteca.models.multimidia.ItemMultimidia;
 
 public abstract class Funcionario extends Membro {
 	public enum Cargo{
@@ -12,7 +16,7 @@ public abstract class Funcionario extends Membro {
 	public Funcionario(String nome, String endereco, String contato, String id) {
 		super(nome, endereco, contato);
 		this.cargo = Cargo.Atendente;
-		this.nivelAcesso = 5;
+		this.nivelAcesso = 5; // Número arbitrátrio que garante que um "funcionário genérico" não tenha permissões
 	}
 
 	public Funcionario.Cargo getCargo() {
@@ -29,5 +33,21 @@ public abstract class Funcionario extends Membro {
 
 	protected void setNivelAcesso(int nivelAcesso) {
 		this.nivelAcesso = nivelAcesso;
+	}
+
+	public void cadastrarItem(ItemMultimidia item, Biblioteca biblioteca){
+
+		try {
+			if (this.nivelAcesso > 1){ // Menos que um gerente
+				throw new AcessoNegadoException("Permissão insuficiente para cadastrar item.");
+			}
+			if (biblioteca.getItens().containsKey(item.getTombo())){
+				throw new ItemInvalidoException("ID já consta na base de dados");
+			}
+			biblioteca.addItem(item);
+			System.out.println("Item cadastrado com sucesso.");
+		} catch (AcessoNegadoException | ItemInvalidoException e){
+			System.err.println("Erro ao cadastrar item:" + e.getMessage());
+		}
 	}
 }
